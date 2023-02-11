a416861eb3c96d8c7a5dc0be3fa8688272c208fc
Paulo Reis <92232479+PauloWarley@users.noreply.github.com>
2023-02-11 17:02:15 -0300

    crud completo

---
+++ api_todo/app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import TodoListAndCreate, TodoDetailChangeAndDelete
+
+urlpatterns = [
+    path('', TodoListAndCreate.as_view()),
+    path('<int:pk>/', TodoDetailChangeAndDelete.as_view())
+]
